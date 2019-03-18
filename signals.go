244b768bd43604e021cdb8c27f4d0ad4c5dab3df
Ben Ransford <ben@virtalabs.com>
2019-03-17 18:05:18 -0700

    handle SIGTERM too

--- signals.go
+++ signals.go
@@ -9,13 +9,14 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"syscall"
 )
 
 // registerInterruptHandler spawns a goroutine that listens for an interrupt
 // signal (e.g., Ctrl-C) and prints the global Stats object to standard output.
 func registerInterruptHandler() {
 	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-sig // eat the signal
 		fmt.Println(stats.String())
