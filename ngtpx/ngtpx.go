051e8849ee7f57994a5540baf93b80a41e9d8d66
Ben Ransford <ben@virtalabs.com>
2019-05-25 23:21:50 -0700

    simple multi-threaded packet parsing

--- ngtpx/ngtpx.go
+++ ngtpx/ngtpx.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"runtime"
 	"sync"
 	"time"
 
@@ -14,75 +15,164 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
-func emitAssets() {
-	log.Println("emitAssets()")
+type ArpTable struct {
+	sync.Mutex
+	arpTable map[string]net.IP
 }
 
-func main() {
-	fileName := flag.String("pcap", "", "pcap file to read")
-	iface := flag.String("iface", "", "interface to listen on")
-	emitInterval := flag.Int("emit-interval", 10,
+// Add adds an ARP table entry.
+func (a *ArpTable) Add(hwAddr net.HardwareAddr, ip net.IP) {
+	a.Lock()
+	defer a.Unlock()
+
+	a.arpTable[string(hwAddr)] = ip
+}
+
+// Print prints the ARP table.
+func (a *ArpTable) Print() {
+	fmt.Printf("ARP table (%d):\n", len(a.arpTable))
+	for mac, ip := range a.arpTable {
+		fmt.Printf(" - %v == %v\n",
+			net.HardwareAddr([]byte(mac)),
+			net.IP(ip))
+	}
+}
+
+// NewArpTable initialized an ARP table.
+func NewArpTable() *ArpTable {
+	at := &ArpTable{}
+	at.arpTable = make(map[string]net.IP)
+	return at
+}
+
+type NetStats struct {
+	sync.Mutex
+	decodingErrors map[string]int
+}
+
+func NewNetStats() *NetStats {
+	ns := &NetStats{}
+	ns.decodingErrors = make(map[string]int)
+	return ns
+}
+
+func (n *NetStats) AddDecodingError(err error) {
+	n.Lock()
+	defer n.Unlock()
+
+	if err != nil {
+		n.decodingErrors[err.Error()]++
+	}
+}
+
+var (
+	fileName     = flag.String("pcap", "", "pcap file to read")
+	iface        = flag.String("iface", "", "interface to listen on")
+	emitInterval = flag.Int("emit-interval", 10,
 		"how often (in seconds) to emit assets to an API endpoint")
+
+	arpTable *ArpTable
+	netStats *NetStats
+)
+
+func main() {
 	flag.Parse()
 
+	// Check that the user provided either a pcap file or an interface on whih to listen (not both)
 	var handle *pcap.Handle
 	var err error
-	if *iface != "" {
-		handle, err = pcap.OpenLive(*iface, 1600, true, pcap.BlockForever)
+	if *iface != "" && *fileName != "" {
+		fmt.Fprintln(os.Stderr, "Specify -iface or -pcap, but not both")
+		os.Exit(1)
+	} else if *iface != "" {
+		if handle, err = pcap.OpenLive(*iface, 1600, true, pcap.BlockForever); err != nil {
+			log.Fatalln(err)
+		}
 	} else if *fileName != "" {
-		handle, err = pcap.OpenOffline(*fileName)
+		if handle, err = pcap.OpenOffline(*fileName); err != nil {
+			log.Fatalln(err)
+		}
 	} else {
 		fmt.Fprintln(os.Stderr, "Specify -iface or -pcap")
 		os.Exit(1)
 	}
-	if err != nil {
-		log.Fatalln(err)
-	}
 
-	fmt.Printf("%T", time.Second)
-	assetEmitter := time.NewTicker(time.Duration(*emitInterval) * time.Second)
-	defer assetEmitter.Stop()
+	arpTable = NewArpTable()
+	defer arpTable.Print()
 	go func() {
-		for range assetEmitter.C {
-			emitAssets()
+		timer := time.NewTicker(5 * time.Second)
+		for range timer.C {
+			arpTable.Print()
 		}
 	}()
 
+	netStats = NewNetStats()
+	defer fmt.Printf("%v\n", netStats)
+	readPacketsFromHandle(handle)
+}
+
+func readPacketsFromHandle(handle *pcap.Handle) {
+	packets := gopacket.NewPacketSource(handle, handle.LinkType())
+	pchan := packets.Packets()
 	var wg sync.WaitGroup
-	wg.Add(1)
-	go readPackets(handle, &wg)
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go readPacketsWithDecodingLayerParser(pchan, &wg)
+	}
 	wg.Wait()
 }
 
-func readPackets(handle *pcap.Handle, wg *sync.WaitGroup) {
+func readPacketsWithDecodingLayerParser(pchan <-chan gopacket.Packet, wg *sync.WaitGroup) {
+	var (
+		eth     layers.Ethernet
+		arp     layers.ARP
+		ip4     layers.IPv4
+		ip6     layers.IPv6
+		udp     layers.UDP
+		tcp     layers.TCP
+		payload gopacket.Payload
+	)
+
+	parser := gopacket.NewDecodingLayerParser(
+		layers.LayerTypeEthernet, // base layer type
+		&eth, &arp, &ip4, &ip6, &udp, &tcp, &payload)
+	decodedLayers := []gopacket.LayerType{}
+
 	defer wg.Done()
-	src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
-	in := src.Packets()
-	var packet gopacket.Packet
-	pkts := 0
-	for {
-		select {
-		case packet = <-in:
-			if packet == nil {
-				return
+
+	for packet := range pchan {
+		err := parser.DecodeLayers(packet.Data(), &decodedLayers)
+		if err != nil {
+			// decoding stack doesn't know how to decode this packet
+			netStats.AddDecodingError(err)
+			continue
+		}
+
+		for _, layerType := range decodedLayers {
+			switch layerType {
+			case layers.LayerTypeARP:
+				if arp.Operation == layers.ARPReply {
+					log.Printf("ARP Reply: %v is at %v\n",
+						net.IP(arp.SourceProtAddress),
+						net.HardwareAddr(arp.SourceHwAddress))
+					arpTable.Add(
+						net.HardwareAddr(arp.SourceHwAddress),
+						net.IP(arp.SourceProtAddress))
+				}
+			case layers.LayerTypeIPv4:
+				log.Printf("Got an IPv4 packet from %s to %s\n",
+					ip4.SrcIP.String(), ip4.DstIP.String())
+			case layers.LayerTypeIPv6:
+				log.Printf("Got an IPv6 packet from %s to %s\n",
+					ip6.SrcIP.String(), ip6.DstIP.String())
+			case layers.LayerTypeTCP:
+				log.Println("Got a TCP packet")
+			case layers.LayerTypeUDP:
+				log.Println("Got a UDP packet")
+			case gopacket.LayerTypePayload:
+				// appLayer := packet.ApplicationLayer()
+				log.Printf("Payload: %d bytes\n", len(payload.Payload()))
 			}
-			arpLayer := packet.Layer(layers.LayerTypeARP)
-			if arpLayer == nil {
-				continue
-			}
-			arp, ok := arpLayer.(*layers.ARP)
-			if !ok {
-				fmt.Println("not ok")
-				continue
-			}
-			if arp.Operation != layers.ARPReply {
-				continue
-			}
-			// Note:  we might get some packets here that aren't responses to ones we've sent,
-			// if for example someone else sends US an ARP request.  Doesn't much matter, though...
-			// all information is good information :)
-			fmt.Printf("%06d: IP %v is at %v\n", pkts, net.IP(arp.SourceProtAddress), net.HardwareAddr(arp.SourceHwAddress))
-			pkts++
 		}
 	}
 }
