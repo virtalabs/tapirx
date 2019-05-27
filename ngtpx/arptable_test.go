234ca1df6c488f406849544627fdb797a7bf50d6
Ben Ransford <ben@virtalabs.com>
2019-05-26 22:53:32 -0700

    add simple ARP table tests

---
+++ ngtpx/arptable_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net"
+	"testing"
+)
+
+func TestArpTableAdd(t *testing.T) {
+	arpTable := NewArpTable()
+
+	arpTable.Add(net.HardwareAddr{1, 2, 3, 4, 5, 6}, net.IP{10, 1, 2, 3})
+	if len(arpTable.arpTable) != 1 {
+		t.Error("Failed to add entry")
+	}
+	arpTable.Add(net.HardwareAddr{1, 2, 3, 4, 5, 6}, net.IP{10, 1, 2, 3})
+	if len(arpTable.arpTable) != 1 {
+		t.Error("Should have overwritten existing entry")
+	}
+}
+
+func BenchmarkArpTableAdd(b *testing.B) {
+	arpTable := NewArpTable()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			arpTable.Add(net.HardwareAddr{1, 2, 3, 4, 5, 6}, net.IP{10, 1, 2, 3})
+		}
+	})
+}
